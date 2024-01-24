d8e0cde32e5e44a7261a37cf19ef675a46e80d56
Ngo Thi Xuan Thuong <thuongngo09072003@gmail.com>
2024-01-24 09:05:07 +0700

    Your commit message

---
+++ B1/BT_Buoi1/bai3/index.js
@@ -0,0 +1,12 @@
+var a = 10
+var b = 25
+var c = a + b
+
+document.write('c= ' + c)
+var x =  100 + 200
+document.write('x= ' + x)
+
+var a = prompt("Nhập chiều dài: ","0" )
+var b = prompt("Nhập chiều rộng: ","0" )
+
+alert("Diện dích là: " + a*b)
